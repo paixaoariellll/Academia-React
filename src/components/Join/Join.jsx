4cf774b07173f46702807ae457b52935a4e23fbc
paixaoariellll <paixaoariellll@gmail.com>
2022-07-15 17:19:16 -0300

    Join + Api de email + Validação do campo email

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Join.css";
+import emailjs from "@emailjs/browser/";
 
 const Join = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_sfomv1p",
+        "template_yyrv61e",
+        form.current,
+        "qJeEVZanCaEJxYXhj"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="Join" id="join-us">
-      <div className="left-j"></div>
-      <div className="rigth-j"></div>
+      <div className="left-j">
+        <hr />
+        <div>
+          <span className="stroke-text">READY TO</span>
+          <span>LEVEL UP</span>
+        </div>
+        <div>
+          <span>YOUR BODY</span>
+          <span className="stroke-text">WITH US?</span>
+        </div>
+      </div>
+      <div className="right-j">
+        <form action="" className="email-container" onSubmit={sendEmail}>
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Digite seu endereço de email"
+          />
+          <button className="btn btn-j">Enviar Agora</button>
+        </form>
+      </div>
     </div>
   );
 };
