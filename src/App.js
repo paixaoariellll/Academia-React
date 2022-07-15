ff6ccb7c86b7ecf1532e9287f0c083067fbc9aa9
paixaoariellll <paixaoariellll@gmail.com>
2022-07-15 00:06:48 -0300

    Plans

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
 import { programsData } from './data/programsData';
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Programs />
       <Reasons />
+      <Plans />
     </div>
   );
 }
---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Plans.css";
+import { plansData } from "../../data/plansData";
+import whiteTick from "../../assets/whiteTick.png";
+
+const Plans = () => {
+  return (
+    <div className="plans-container">
+      <div className="programs-header">
+        <span className="stroke-text">Read to Start</span>
+        <span>Your Journey</span>
+        <span className="stroke-text">Now With Us!</span>
+      </div>
+
+      {/* Plans cards */}
+      <div className="plans">
+        {plansData.map((plan, i) => (
+          <div className="plan" key={i}>
+            {plan.icon}
+            <span>$ {plan.name}</span>
+            <span>$ {plan.price}</span>
+
+            <div className="features">
+              {plan.features.map((feature, i) => (
+                <div className="feature">
+                  <img src={whiteTick} alt="white Tick" />
+                  <span key={i}>{feature}</span>
+                </div>
+              ))}
+            </div>
+
+            <div>
+              <span>See more benefits ✅ </span>
+            </div>
+            <button className="btn">Join Now</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Plans;
